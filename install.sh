5e51a31671a6beda1e0a286da5475f3c796bdefe
flaviogf <flavio.fernandes6@gmail.com>
2019-03-26 22:40:35 -0300

    zsh

--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
 # git
-apt install git -y
+echo "Instalando git"
+apt install git -yq
 
 # neovim
-apt install neovim -y
+echo "Instalando neovim"
+apt install neovim -yq
 
 # zsh
-apt install zsh -y
+echo "Instalando zsh"
+apt install zsh -yq
+echo "Configurando zsh"
+chsh -s /bin/zsh
+
+# oh-my-zsh
+echo "Instalando oh my zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # chromium
+echo "Instalando chromium"
 snap install chromium
 
 # vscode
+echo "Instalando vscode"
 snap install vscode --classic
 
 # tmux
+echo "Instalando tmux"
 snap install tmux --classic
