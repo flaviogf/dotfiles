e5f8a9dc1e070846b4660e17da405a82560582d4
Flavio <flavio.fernandes6@gmail.com>
2019-03-30 10:10:15 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -16,7 +16,6 @@ zshrc() {
 	local ARQUIVO="$HOME/.zshrc"
 	local URL="https://raw.githubusercontent.com/flaviogf/dotfiles/master/zsh/.zshrc"
 	curl -fLo $ARQUIVO --create-dirs $URL
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 }
