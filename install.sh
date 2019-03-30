f0d9ef99e49961365938eac829026ac8e27672dc
Flavio <flavio.fernandes6@gmail.com>
2019-03-30 09:57:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,14 +10,6 @@ apt install neovim -y
 echo "Instalando zsh"
 apt install zsh -y
 
-# oh-my-zsh
-echo "Instalando oh my zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo "Instalando plugins zsh"
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
 # chromium
 echo "Instalando chromium"
 snap install chromium
