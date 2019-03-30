2b7f309e8bee05e0d2e0da7332ee1931cb399f3f
Flavio <flavio.fernandes6@gmail.com>
2019-03-30 08:10:55 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,14 +2,12 @@
 vim_plug() {
 	local ARQUIVO="$HOME/.local/share/nvim/site/autoload/plug.vim"
 	local URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-
 	curl -fLo $ARQUIVO --create-dirs $URL
 }
 
 init_vim() {
 	local ARQUIVO="$HOME/.config/nvim/init.vim"
 	local URL="https://raw.githubusercontent.com/flaviogf/dotfiles/master/nvim/init.vim"
-
 	curl -fLo $ARQUIVO --create-dirs $URL
 }
 
