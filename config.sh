ca5c97dd5c070715bf5f4b6b79704e3971c63afa
Flavio <flavio.fernandes6@gmail.com>
2019-04-19 07:34:43 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
 # neovim
-vim_plug() {
+config_vim_plug() {
 	local ARQUIVO="$HOME/.local/share/nvim/site/autoload/plug.vim"
 	local URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 	curl -fLo $ARQUIVO --create-dirs $URL
 }
 
-init_vim() {
+config_nvim() {
 	local ARQUIVO="$HOME/.config/nvim/init.vim"
 	local URL="https://raw.githubusercontent.com/flaviogf/dotfiles/master/nvim/init.vim"
 	curl -fLo $ARQUIVO --create-dirs $URL
 }
 
 # zsh
-zsh() {
+config_zsh() {
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	local ARQUIVO="$HOME/.zshrc"
 	local URL="https://raw.githubusercontent.com/flaviogf/dotfiles/master/zsh/.zshrc"
@@ -21,6 +21,6 @@ zsh() {
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 }
 
-vim_plug
-init_vim
-zsh
+config_vim_plug
+config_nvim
+config_zsh
