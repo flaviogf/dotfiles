312663e7c9630b054033535f1839f767996d98db
flaviogf <flavio.fernandes6@gmail.com>
2019-07-14 13:12:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,36 @@
 # git
-echo "Instalando git"
+echo "Installing git"
 apt install git -y
 
+# curl
+echo "Installing curl"
+apt install curl -y
+
 # build-essential
-echo "Installando build-essential"
+echo "Installing build-essential"
 apt install build-essential -y
 
 # python
-echo "Installando python3.7"
-apt install python3.7 python3.7-dev python3.7-venv
+echo "Installing python3.7"
+apt install python3.7 python3.7-dev python3.7-venv -y
 
 # neovim
-echo "Instalando neovim"
+echo "Installing neovim"
 apt install neovim -y
 
 # zsh
-echo "Instalando zsh"
+echo "Installing zsh"
 apt install zsh -y
 
 # chromium
-echo "Instalando chromium"
+echo "Installing chromium"
 snap install chromium
 
 # tmux
-echo "Instalando tmux"
+echo "Installing tmux"
 snap install tmux --classic
 
 # vscode
-echo "Instalando vscode"
+echo "Installing vscode"
 snap install code --classic
+
