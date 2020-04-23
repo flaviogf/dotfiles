462da1d4497cd2f017dd3c1e3c6c5f43d7a3a8a4
flaviogf <flavio.fernandes6@gmail.com>
2020-04-22 22:06:19 -0300

    feat: Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,41 @@
-function Invoke-Haskell {
-    docker run -it --rm haskell:8 $args
+function Get-LocationUnixFormat {
+    $location = $(pwd) -replace "\\", "/" 
+    $location -replace "C:", "//c"
 }
+
+function Invoke-DockerRun {
+    $location = Get-LocationUnixFormat
+    docker run -it --rm -v ${location}:/workdir -w /workdir $args
+}
+
+function Invoke-Haskell {
+    Invoke-DockerRun haskell:8 haskell $args
+}
+
+function Invoke-Java {
+    Invoke-DockerRun openjdk:8u252-jdk java $args
+}
+
+function Invoke-Maven {
+    Invoke-DockerRun maven:3.6.3-openjdk-8 mvn $args
+}
+
+function Invoke-Node {
+    Invoke-DockerRun node:12.16.2 node $args
+}
+
+function Invoke-Python {
+    Invoke-DockerRun python:3.8.2 node $args
+}
+
+Set-Alias pwdunix Get-LocationUnixFormat
 
 Set-Alias haskell Invoke-Haskell
 
-function Invoke-Java {
-    docker run -it --rm openjdk:8u252-jdk java $args
-}
-
 Set-Alias java Invoke-Java
 
-function Invoke-Node {
-    docker run -it --rm node:12.16.2 node $args
-}
+Set-Alias mvn Invoke-Maven
 
 Set-Alias node Invoke-Node
-
-function Invoke-Python {
-    docker run -it --rm python:3.8.2 python $args
-}
 
 Set-Alias python Invoke-Python
