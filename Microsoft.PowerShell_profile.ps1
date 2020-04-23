52495805cc38bc7d0a8f049758e034e40b47b3d0
flaviogf <flavio.fernandes6@gmail.com>
2020-04-22 22:08:37 -0300

    feat: Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,29 +12,17 @@ function Invoke-Haskell {
     Invoke-DockerRun haskell:8 haskell $args
 }
 
-function Invoke-Java {
-    Invoke-DockerRun openjdk:8u252-jdk java $args
-}
-
-function Invoke-Maven {
-    Invoke-DockerRun maven:3.6.3-openjdk-8 mvn $args
-}
-
 function Invoke-Node {
     Invoke-DockerRun node:12.16.2 node $args
 }
 
 function Invoke-Python {
-    Invoke-DockerRun python:3.8.2 node $args
+    Invoke-DockerRun python:3.8.2 python $args
 }
 
 Set-Alias pwdunix Get-LocationUnixFormat
 
 Set-Alias haskell Invoke-Haskell
-
-Set-Alias java Invoke-Java
-
-Set-Alias mvn Invoke-Maven
 
 Set-Alias node Invoke-Node
 
