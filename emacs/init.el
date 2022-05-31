e1bae6546442facee4819101846068d89135d998
flaviogf <flavio.fernandes6@gmail.com>
2022-05-30 21:05:47 -0300

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -165,16 +165,3 @@
              :init
              (setq which-key-idle-delay 2)
              (which-key-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(edwina which-key vterm use-package seeing-is-believing rspec-mode projectile magit lsp-mode inf-ruby exwm evil-collection doom-themes doom-modeline counsel company all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
