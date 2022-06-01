386895147c7d696c4a0cb96d35780921aee2b350
flaviogf <flavio.fernandes6@gmail.com>
2022-06-01 09:34:42 -0300

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -3,8 +3,6 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'exec-path "/home/flaviogf/.asdf/shims")
-(add-to-list 'exec-path "/home/flaviogf/.asdf/bin")
 
 (package-initialize)
 
@@ -16,15 +14,13 @@
 
 (require 'use-package)
 
-(set-face-attribute 'default nil :font "FiraCode Nerd Font" :height 150)
-
-(global-set-key (kbd "C-x b") 'counsel-switch-buffer)
-(global-set-key (kbd "C-M-u") 'universal-argument)
-
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'hl-line-mode)
 (add-hook 'prog-mode-hook #'whitespace-mode)
+
+(add-to-list 'exec-path "/home/flaviogf/.asdf/shims")
+(add-to-list 'exec-path "/home/flaviogf/.asdf/bin")
 
 (setq-default c-basic-offset 2)
 (setq-default cperl-indent-level 2)
@@ -40,46 +36,25 @@
 (setq-default whitespace-line-column 80)
 (setq-default whitespace-style '(face lines-tail))
 
+(set-face-attribute 'default nil :font "FiraCode Nerd Font" :height 150)
+
 (column-number-mode)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
 
-(defun efs/update-class-name ()
-  (exwm-workspace-rename-buffer exwm-class-name))
-
-(defun efs/update-title ()
-  (exwm-workspace-rename-buffer exwm-title))
-
-(defun efs/run-in-background (command)
-  (let ((command-parts (split-string command "[ ]+")))
-    (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
-
-(defun efs/exwm-init-hook ()
-  (efs/run-in-background "nm-applet")
-  (efs/run-in-background "pasystray")
-  (efs/run-in-background "blueman-applet"))
-
-(use-package edwina
-             :config
-             (setq display-buffer-base-action '(display-buffer-below-selected))
-             (edwina-mode 1))
-
 (use-package exwm
              :config
              (setq exwm-workspace-number 4)
-             (require 'exwm-systemtray)
-             (setq exwm-systemtray-height 22)
-             (exwm-systemtray-enable)
-             (add-hook 'exwm-update-class-hook #'efs/update-class-name)
-             (add-hook 'exwm-update-title-hook #'efs/update-title)
-             (add-hook 'exwm-init-hook #'efs/exwm-init-hook)
              (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
              (exwm-input-set-key (kbd "s-SPC") 'counsel-linux-app)
              (exwm-input-set-key (kbd "s-r") 'exwm-reset)
              (exwm-input-set-key (kbd "s-w") 'exwm-workspace-switch)
-             (exwm-enable))
+             (exwm-enable)
+             :hook
+             (exwm-update-class . (lambda () (exwm-workspace-rename-buffer exwm-class-name)))
+             (exwm-update-title . (lambda () (exwm-workspace-rename-buffer exwm-title))))
 
 (use-package all-the-icons)
 
@@ -88,8 +63,9 @@
              (company-mode))
 
 (use-package counsel
-             :custom (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-             :config (counsel-mode))
+             :init
+             (setq counsel-linux-app-format-function 'counsel-linux-app-format-function-name-only)
+             (counsel-mode))
 
 (use-package doom-themes
              :init
@@ -98,59 +74,56 @@
              (load-theme 'doom-dracula t))
 
 (use-package doom-modeline
-             :init (doom-modeline-mode))
+             :init
+             (doom-modeline-mode))
+
+(use-package edwina
+             :init
+             (setq display-buffer-base-action '(display-buffer-below-selected))
+             (edwina-mode))
 
 (use-package evil
              :init
-             (setq evil-want-keybinding nil)
-             (setq evil-want-integration t)
-             (evil-mode)
-             :custom
-             (evil-want-C-u-scroll 1)
-             (evil-shift-width 2))
-
-(use-package evil-collection
-             :init (evil-collection-init))
+             (setq evil-shift-width 2)
+             (evil-mode))
 
 (use-package ivy
-             :init (ivy-mode))
+             :init
+             (ivy-mode))
 
 (use-package lsp-mode
-             :init
-             (unless (display-graphic-p) (setq lsp-headerline-breadcrumb-icons-enable nil))
-             (setq lsp-keymap-prefix "C-c l")
+             :commands
+             lsp
              :hook
              (ruby-mode . lsp)
-             (lsp-mode . lsp-enable-which-key-integration)
-             :commands lsp)
+             :init
+             (setq lsp-headerline-breadcrumb-icons-enable nil)
+             (setq lsp-keymap-prefix "C-c l"))
 
-(use-package magit
-             :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+(use-package magit)
 
 (use-package perspective
              :bind
              ("C-x b" . persp-counsel-switch-buffer)
-             :custom
-             (persp-mode-prefix-key (kbd "C-c M-p"))
              :init
+             (setq persp-mode-prefix-key (kbd "C-c M-p"))
              (persp-mode))
 
 (use-package projectile
+             :bind
+             (:map projectile-mode-map ("C-c p" . projectile-command-map))
              :init
-             (setq projectile-switch-project-action #'projectile-dired)
              (setq projectile-project-search-path '(("~/dev" . 1)))
              (setq projectile-completion-system 'ivy)
              (setq projectile-git-submodule-command nil)
-             (projectile-mode)
-             :bind (:map projectile-mode-map ("C-c p" . projectile-command-map)))
+             (projectile-mode))
 
 (use-package rspec-mode
              :init
              (setq rspec-use-docker-when-possible t)
              (setq rspec-docker-command "docker-compose exec")
              (setq rspec-docker-container "web")
-             (setq rspec-docker-cwd "/var/app/")
-             (add-hook 'after-init-hook 'inf-ruby-switch-setup))
+             (setq rspec-docker-cwd "/var/app/"))
 
 (use-package vterm)
 
