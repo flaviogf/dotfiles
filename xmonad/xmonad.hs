9a314e38d380979006801699b299b55d339488cf
flaviogf <flavio.fernandes6@gmail.com>
2022-06-03 19:45:39 -0300

    xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,13 @@
 import XMonad
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
-main = xmonad $ def
+main = xmonad $ ewmh $ def
     { modMask = mod4Mask
     }
   `additionalKeysP`
     [ ("M-f", spawn "firefox")
     , ("M-e", spawn "emacs")
+    , ("M-C-s", unGrab *> spawn "scrot -s")
     ]
