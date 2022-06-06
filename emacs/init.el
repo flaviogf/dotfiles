d5c0f5c369e589ff556d48d076f2aca9a3f96784
flaviogf <flavio.fernandes6@gmail.com>
2022-06-06 19:56:22 -0300

    create folders

--- emacs/init.el
+++ emacs/init.el
@@ -154,3 +154,16 @@
              :ensure nil
              :hook
              (prog-mode . whitespace-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(go-mode all-the-icons-dired which-key vterm use-package seeing-is-believing rspec-mode rainbow-mode projectile perspective magit lsp-mode haskell-mode exwm evil-collection edwina doom-themes doom-modeline desktop-environment counsel company all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
