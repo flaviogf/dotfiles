f36a2742df2eba2ce0d7d21b6f59f0ee6ee877eb
flaviogf <flavio.fernandes6@gmail.com>
2022-06-12 16:47:00 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@
 rm -rf /home/flaviogf/.xmonad &&
     mkdir -p /home/flaviogf/.xmonad &&
     ln -s /home/flaviogf/dev/dotfiles/xmonad/xmonad.hs /home/flaviogf/.xmonad/xmonad.hs
+
+# Xmobar
+rm -rf /home/flaviogf/.xmobarrc &&
+    ln -s /home/flaviogf/dev/dotfiles/xmobar/.xmobarrc /home/flaviogf/.xmobarrc
