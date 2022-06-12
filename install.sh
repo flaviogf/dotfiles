171f47a2c793b67633cd5e0488c0de781d7331e2
flaviogf <flavio.fernandes6@gmail.com>
2022-06-12 16:34:30 +0000

    install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Xmonad
+rm -rf /home/flaviogf/.xmonad &&
+    mkdir -p /home/flaviogf/.xmonad &&
+    ln -s /home/flaviogf/dev/dotfiles/xmonad/xmonad.hs /home/flaviogf/.xmonad/xmonad.hs
