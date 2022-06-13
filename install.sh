19952eb1487e06739cdeb622558dd4fedde91823
flaviogf <flavio.fernandes6@gmail.com>
2022-06-13 12:01:32 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ ln -sf "${DOTFILES}/alacritty/.alacritty.yml" "${HOME}/.alacritty.yml"
 # Emacs
 mkdir -p "${HOME}/.emacs.d" && ln -sf "${DOTFILES}/emacs/init.el" "${HOME}/.emacs.d/init.el"
 
+# Neovim
+mkdir -p "${HOME}/.config/nvim" && ln -sf "${DOTFILES}/nvim/init.vim" "${HOME}/.config/nvim/init.vim"
+
 # Xmonad
 mkdir -p "${HOME}/.xmonad" && ln -sf "${DOTFILES}/xmonad/xmonad.hs" "${HOME}/.xmonad/xmonad.hs"
 
