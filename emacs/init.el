c7d724ecfbe99d03ee8d11424ec1689b83a2a441
flaviogf <flavio.fernandes6@gmail.com>
2022-06-23 21:20:17 -0300

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -41,145 +41,141 @@
 (use-package all-the-icons)
 
 (use-package all-the-icons-dired
-             :hook
-             (dired-mode . all-the-icons-dired-mode))
+  :hook
+  (dired-mode . all-the-icons-dired-mode))
 
 (use-package company
-             :init
-             (company-mode))
+  :init
+  (company-mode))
 
 (use-package counsel
-             :init
-             (counsel-mode))
+  :init
+  (counsel-mode))
 
 (use-package display-fill-column-indicator
-             :ensure nil
-             :hook
-             (prog-mode . display-fill-column-indicator-mode))
+  :ensure nil
+  :hook
+  (prog-mode . display-fill-column-indicator-mode))
 
 (use-package display-line-numbers
-             :ensure nil
-             :hook
-             (prog-mode . display-line-numbers-mode))
+  :ensure nil
+  :hook (prog-mode . display-line-numbers-mode))
 
 (use-package doom-themes
-             :init
-             (setq doom-themes-enable-bold t)
-             (setq doom-themes-enable-italic t)
-             (load-theme 'doom-dracula t))
+  :custom
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
+  :init
+  (load-theme 'doom-dracula t))
 
 (use-package doom-modeline
-             :init
-             (doom-modeline-mode))
+  :init
+  (doom-modeline-mode))
 
 (use-package editorconfig
-             :init
-             (editorconfig-mode))
+  :init
+  (editorconfig-mode))
 
 (use-package elfeed
-             :init
-             (setq elfeed-search-filter "@1-week-ago +unread")
-             (setq elfeed-feeds '(("jovemnerd.com.br/feed/" news)
-                                  ("blog.digitalocean.com/rss/" tech))))
+  :custom
+  (elfeed-search-filter "@1-week-ago +unread")
+  (elfeed-feeds '(("jovemnerd.com.br/feed/" news)
+                  ("blog.digitalocean.com/rss/" tech))))
 
 (use-package evil
-             :init
-             (setq evil-shift-width 2)
-             (setq evil-want-C-u-scroll t)
-             (setq evil-want-integration t)
-             (setq evil-want-keybinding nil)
-             (evil-mode))
+  :custom
+  (evil-shift-width 2)
+  (evil-want-C-u-scroll t)
+  (evil-want-integration t)
+  (evil-want-keybinding nil)
+  :init
+  (evil-mode))
 
 (use-package evil-collection
-             :after evil
-             :init
-             (evil-collection-init))
+  :after evil
+  :init (evil-collection-init))
 
 (use-package git-gutter
-             :hook
-             (prog-mode . git-gutter-mode))
+  :hook
+  (prog-mode . git-gutter-mode))
 
 (use-package go-mode
-             :init
-             (setq gofmt-command "goimports"))
+  :custom
+  (gofmt-command "goimports"))
 
 (use-package haskell-mode)
 
 (use-package hl-line
-             :ensure nil
-             :hook
-             (prog-mode . hl-line-mode))
+  :ensure nil
+  :hook
+  (prog-mode . hl-line-mode))
 
 (use-package ivy
-             :init
-             (ivy-mode))
+  :init
+  (ivy-mode))
 
 (use-package lsp-mode
-             :commands
-             lsp
-             :hook
-             (ruby-mode . lsp)
-             (go-mode . lsp)
-             :init
-             (setq lsp-diagnostics-provider :none)
-             (setq lsp-eldoc-enable-hover nil)
-             (setq lsp-headerline-breadcrumb-enable nil)
-             (setq lsp-keymap-prefix "C-c l")
-             (setq lsp-modeline-code-actions-enable nil)
-             (setq lsp-ui-sideline-enable nil))
+  :commands lsp
+  :custom
+  (lsp-diagnostics-provider :none)
+  (lsp-eldoc-enable-hover nil)
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-keymap-prefix "C-c l")
+  (lsp-modeline-code-actions-enable nil)
+  (lsp-ui-sideline-enable nil)
+  :hook
+  (go-mode . lsp)
+  (ruby-mode . lsp))
 
 (use-package magit)
 
 (use-package org
-             :ensure nil
-             :init
-             (setq org-hide-emphasis-markers t))
+  :ensure nil
+  :custom
+  (org-hide-emphasis-markers t))
 
 (use-package org-superstar
-             :hook
-             (org-mode . org-superstar-mode)
-             :init
-             (setq org-superstar-remove-leading-stars t)
-             (setq org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●")))
+  :custom
+  (org-superstar-remove-leading-stars t)
+  (org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●"))
+  :hook
+  (org-mode . org-superstar-mode))
 
 (use-package projectile
-             :bind
-             (:map projectile-mode-map ("C-c p" . projectile-command-map))
-             :init
-             (setq projectile-completion-system 'ivy)
-             (setq projectile-git-submodule-command nil)
-             (setq projectile-project-search-path '(("~/dev" . 1)))
-             (setq projectile-switch-project-action 'projectile-dired)
-             (projectile-mode))
+  :bind
+  (:map projectile-mode-map ("C-c p" . projectile-command-map))
+  :custom
+  (projectile-completion-system 'ivy)
+  (projectile-git-submodule-command nil)
+  (projectile-project-search-path '(("~/dev" . 1)))
+  (projectile-switch-project-action 'projectile-dired)
+  :init
+  (projectile-mode))
 
 (use-package rainbow-mode
-             :hook
-             (conf-unix-mode . rainbow-mode)
-             (prog-mode . rainbow-mode))
+  :hook
+  (conf-unix-mode . rainbow-mode)
+  (prog-mode . rainbow-mode))
 
 (use-package rg)
 
-(use-package rspec-mode
-             :init
-             (setq rspec-use-docker-when-possible t)
-             (setq rspec-docker-command "docker-compose exec")
-             (setq rspec-docker-container "web")
-             (setq rspec-docker-cwd "/var/app/"))
+(use-package rspec-mode)
 
 (use-package slime
-             :init
-             (setq inferior-lisp-program "sbcl"))
+  :custom
+  (inferior-lisp-program "sbcl"))
 
 (use-package vterm)
 
 (use-package which-key
-             :init
-             (setq which-key-idle-delay 2)
-             (which-key-mode))
+  :custom
+  (which-key-idle-delay 2)
+  :init
+  (which-key-mode))
 
 (use-package whitespace
-             :ensure nil
-             :hook
-             (prog-mode . whitespace-mode))
+  :ensure nil
+  :hook
+  (prog-mode . whitespace-mode))
 
 (use-package yaml-mode)
