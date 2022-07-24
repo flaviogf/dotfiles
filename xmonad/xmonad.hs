6b42aef964f26c8a7fe55787753ed33758855d8b
flaviogf <flavio.fernandes6@gmail.com>
2022-07-24 16:11:15 -0300

    i3
    xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -62,7 +62,7 @@ myTerminal = "alacritty"
 myWindowCount :: X (Maybe String)
 myWindowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
-myWorkspaces = [" www ", " dev ", " com ", " chat ", " gam ", " sys "]
+myWorkspaces = [" www ", " dev ", " job ", " chat ", " gam ", " sys "]
 
 myLayoutHook = avoidStruts
                $ withBorder myBorderWidth
