492736a9c4cd23f0ccf2de1fc4c10641206514f1
flaviogf <flavio.fernandes6@gmail.com>
2022-08-02 13:22:21 -0300

    neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,24 @@
-vim.o.clipboard = 'unnamedplus'
-vim.o.colorcolumn = '80,120'
+vim.g.mapleader = ','
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.colorcolumn = '80,120'
+vim.opt.cursorline = true
+vim.opt.encoding = 'utf-8'
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.history = 200
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.wildmenu = true
+vim.opt.wildmode = full
+vim.opt.wrap = false
