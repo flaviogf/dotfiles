020af637e4a658e4e46d54dcf7714fbbe5369867
flaviogf <flavio.fernandes6@gmail.com>
2022-08-02 21:04:34 -0300

    neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 require('packer').startup(function(use)
   use { 'dracula/vim' }
+  use { 'editorconfig/editorconfig-vim' }
   use { 'kyazdani42/nvim-web-devicons' }
   use { 'nvim-lualine/lualine.nvim' }
   use { 'tpope/vim-dispatch' }
@@ -13,29 +14,35 @@ require('lualine').setup({
   },
 })
 
-vim.g.mapleader = ','
+local keymap = vim.api.nvim_set_keymap
 
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.colorcolumn = '80,120'
-vim.opt.cursorline = true
-vim.opt.encoding = 'utf-8'
-vim.opt.expandtab = true
-vim.opt.hidden = true
-vim.opt.history = 200
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.mouse = 'a'
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 2
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.softtabstop = 2
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.tabstop = 2
-vim.opt.wildmenu = true
-vim.opt.wildmode = full
-vim.opt.wrap = false
+local set = vim.opt
 
 vim.cmd "colorscheme dracula"
+
+vim.g.mapleader = ','
+
+keymap('v', 'p', '"_dP', { noremap = true, silent = true })
+
+set.clipboard = 'unnamedplus'
+set.colorcolumn = '80,120'
+set.cursorline = true
+set.encoding = 'utf-8'
+set.expandtab = true
+set.hidden = true
+set.history = 200
+set.ignorecase = true
+set.incsearch = true
+set.mouse = 'a'
+set.number = true
+set.relativenumber = true
+set.shiftwidth = 2
+set.smartcase = true
+set.smartindent = true
+set.softtabstop = 2
+set.splitbelow = true
+set.splitright = true
+set.tabstop = 2
+set.wildmenu = true
+set.wildmode = full
+set.wrap = false
