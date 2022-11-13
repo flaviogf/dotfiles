5efa1ada94bcf5205fcf7f92d9ba651336a86be3
flaviogf <flavio.fernandes6@gmail.com>
2022-11-13 14:58:03 -0300

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,7 +43,7 @@ cmp.setup({
 hop.setup()
 
 lspconfig['solargraph'].setup({
-  capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities()),
   on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true, buffer = bufnr }
     keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
