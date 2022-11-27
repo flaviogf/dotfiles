80137680223333217b9b382f72cde4b3501f28cc
flaviogf <flavio.fernandes6@gmail.com>
2022-11-27 18:15:30 -0300

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set fish_greeting
 
-set -Ux EDITOR "emacsclient --socket-name=emacs --tty"
 set -Ux VISUAL "emacsclient --socket-name=emacs --create-frame"
+set -Ux EDITOR "nvim"
 
 . $HOME/.asdf/asdf.fish
 
