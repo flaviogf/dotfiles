2a8d149f3f540c9144e4d1afdb589141305b09d7
flaviogf <flavio.fernandes6@gmail.com>
2022-11-27 09:18:29 -0300

    install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DOTFILES="${HOME}/dev/dotfiles"
-
-# Starship
-ln -sf "${DOTFILES}/starship/starship.toml" "${HOME}/.config/starship.toml"
-
-# Xorg
-ln -sf "${DOTFILES}/xorg/.xinitrc" "${HOME}/.xinitrc"
