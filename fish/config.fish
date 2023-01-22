c91c7dede3088b1ef68e1e9050344a73a5a70cdb
flaviogf <flavio.fernandes6@gmail.com>
2023-01-22 12:05:25 -0300

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 set fish_greeting
 
-set -Ux VISUAL "emacsclient --socket-name=emacs --create-frame"
-set -Ux EDITOR "emacsclient --socket-name=emacs --tty"
+set -x VISUAL "emacsclient --socket-name=emacs --create-frame"
+set -x EDITOR "emacsclient --socket-name=emacs --tty"
+
+set -x DISPLAY $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+set -x LIBGL_ALWAYS_INDIRECT 1
 
 . $HOME/.asdf/asdf.fish
 
