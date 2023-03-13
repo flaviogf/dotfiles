904b23cbdb6f7fcdb3e97a0bb82c1a82ef126402
flaviogf <flavio.fernandes6@gmail.com>
2023-03-13 14:16:33 -0300

    neovim

---
+++ neovim/lua/user/lualine.lua
@@ -0,0 +1,13 @@
+local ok, lualine = pcall(require, 'lualine')
+
+if not ok then
+  return
+end
+
+lualine.setup({
+  options = {
+    component_separators = '',
+    section_separators = '',
+    theme = 'nord',
+  },
+})
