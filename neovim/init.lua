39b95902c9a00e57bb6c45c9de34430a12d8b346
flaviogf <flavio.fernandes6@gmail.com>
2023-03-13 14:09:57 -0300

    neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,7 +1,8 @@
+require 'user.gitsigns'
+require 'user.options'
+
 local cmd = vim.cmd
-local g = vim.g
 local keymap = vim.keymap.set
-local set = vim.opt
 
 cmd('packadd packer.nvim')
 
@@ -25,8 +26,6 @@ require('packer').startup(function(use)
   use { 'hrsh7th/nvim-cmp' }
   use { 'hrsh7th/cmp-nvim-lsp' }
 end)
-
-require('gitsigns').setup({})
 
 require('lualine').setup({
   options = {
@@ -88,33 +87,3 @@ cmp.setup({
 })
 
 cmd("colorscheme nord")
-
-g.mapleader = ','
-
-local opts = { noremap = true, silent = true }
-keymap('v', 'p', '"_dP', opts)
-
-set.clipboard = 'unnamedplus'
-set.cursorline = true
-set.encoding = 'utf-8'
-set.expandtab = true
-set.hidden = true
-set.history = 200
-set.ignorecase = true
-set.incsearch = true
-set.mouse = 'a'
-set.number = true
-set.relativenumber = true
-set.scrolloff = 8
-set.shiftwidth = 2
-set.sidescrolloff = 8
-set.smartcase = true
-set.smartindent = true
-set.softtabstop = 2
-set.splitbelow = true
-set.splitright = true
-set.tabstop = 2
-set.termguicolors = true
-set.wildmenu = true
-set.wildmode = 'full'
-set.wrap = false
---
+++ neovim/lua/user/options.lua
@@ -0,0 +1,33 @@
+local g = vim.g
+local keymap = vim.keymap.set
+local set = vim.opt
+
+g.mapleader = ','
+
+set.clipboard = 'unnamedplus'
+set.cursorline = true
+set.encoding = 'utf-8'
+set.expandtab = true
+set.hidden = true
+set.history = 200
+set.ignorecase = true
+set.incsearch = true
+set.mouse = 'a'
+set.number = true
+set.relativenumber = true
+set.scrolloff = 8
+set.shiftwidth = 2
+set.sidescrolloff = 8
+set.smartcase = true
+set.smartindent = true
+set.softtabstop = 2
+set.splitbelow = true
+set.splitright = true
+set.tabstop = 2
+set.termguicolors = true
+set.wildmenu = true
+set.wildmode = 'full'
+set.wrap = false
+
+local opts = { noremap = true, silent = true }
+keymap('v', 'p', '"_dP', opts)
