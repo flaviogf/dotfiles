12c5f517971f1cacabe9d0e66b80ff542493664f
flaviogf <flavio.fernandes6@gmail.com>
2023-03-13 14:21:32 -0300

    neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -11,24 +11,19 @@ cmd('packadd packer.nvim')
 cmd("colorscheme nord")
 
 require('packer').startup(function(use)
-  use { 'wbthomason/packer.nvim' }
-
-  use { 'nvim-lua/plenary.nvim' }
-
   use { 'arcticicestudio/nord-vim' }
   use { 'editorconfig/editorconfig-vim' }
-  use { 'lewis6991/gitsigns.nvim' }
-  use { 'nvim-lualine/lualine.nvim' }
-  use { 'nvim-tree/nvim-tree.lua' }
-  use { 'nvim-treesitter/nvim-treesitter' }
-
-  use { 'nvim-telescope/telescope.nvim' }
-
-  use { 'neovim/nvim-lspconfig' }
-  use { 'mfussenegger/nvim-jdtls' }
-
   use { 'hrsh7th/nvim-cmp' }
   use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'lewis6991/gitsigns.nvim' }
+  use { 'mfussenegger/nvim-jdtls' }
+  use { 'neovim/nvim-lspconfig' }
+  use { 'nvim-lua/plenary.nvim' }
+  use { 'nvim-lualine/lualine.nvim' }
+  use { 'nvim-telescope/telescope.nvim' }
+  use { 'nvim-tree/nvim-tree.lua' }
+  use { 'nvim-treesitter/nvim-treesitter' }
+  use { 'wbthomason/packer.nvim' }
 end)
 
 require('nvim-tree').setup({})
