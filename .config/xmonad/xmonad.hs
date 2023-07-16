828511ff118450f662894a786866a66c38e82eb5
flaviogf <flavio.fernandes6@gmail.com>
2023-07-15 21:20:12 -0300

    xmonad

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -4,26 +4,6 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
-colorBack = "#2E3440"
-colorFore = "#D8DEE9"
-color01 = "#3B4252"
-color02 = "#BF616A"
-color03 = "#A3BE8C"
-color04 = "#EBCB8B"
-color05 = "#81A1C1"
-color06 = "#B48EAD"
-color07 = "#88C0D0"
-color08 = "#E5E9F0"
-color09 = "#4C566A"
-color10 = "#BF616A"
-color11 = "#A3BE8C"
-color12 = "#EBCB8B"
-color13 = "#81A1C1"
-color14 = "#B48EAD"
-color15 = "#8FBCBB"
-color16 = "#ECEFF4"
-colorTrayer = "--tint 0x2E3440"
-
 myBrowser :: String
 myBrowser = "google-chrome-stable"
 
@@ -34,10 +14,10 @@ myBorderWidth :: Dimension
 myBorderWidth = 2
 
 myNormalColor :: String
-myNormalColor = colorBack
+myNormalColor = "#2E3440"
 
 myFocusColor :: String
-myFocusColor = color15
+myFocusColor = "#8FBCBB"
 
 main :: IO ()
 
