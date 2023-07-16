8508551d2449f4180590f09fd65be4ed79fd0f84
flaviogf <flavio.fernandes6@gmail.com>
2023-07-16 13:21:52 -0300

    neovim

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -2,9 +2,15 @@ local bundles = { vim.fn.glob('~/.vscode/extensions/vscjava.vscode-java-debug-*/
 
 vim.list_extend(bundles, vim.split(vim.fn.glob('~/.vscode/extensions/vscjava.vscode-java-test-*/server/*.jar', 1), '\n'))
 
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+local configuration = '/home/flaviogf/dev/' .. project_name .. '/config-linux'
+
+local data = '/home/flaviogf/dev/' .. project_name
+
 require('jdtls').start_or_attach({
   on_attach = function(_, _) require('jdtls').setup_dap({ hotcodereplace = 'auto' }) end,
   root_dir = require('jdtls.setup').find_root({ 'mvnw' }),
   init_options = { bundles = bundles },
-  cmd = { 'jdtls' },
+  cmd = { 'jdt-language-server', '-configuration', configuration, '-data', data },
 })
