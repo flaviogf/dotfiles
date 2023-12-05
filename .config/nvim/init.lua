61d65f273ec6640c98f6d57477f8435746d20c66
flaviogf <flavio.fernandes6@gmail.com>
2023-12-05 15:53:24 -0300

    neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,61 +1,76 @@
-require 'user.cmp'
-require 'user.colorizer'
-require 'user.gitsigns'
-require 'user.keymaps'
-require 'user.lsp'
-require 'user.lualine'
-require 'user.options'
-require 'user.telescope'
-require 'user.toggleterm'
-require 'user.tree'
-require 'user.treesitter'
-require 'user.which_key'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
-local path = '/home/flaviogf/.local/share/nvim/site/pack/packer/start/packer.nvim'
-
-local cmd = vim.cmd
-
-local fn = vim.fn
-
-local setup = function()
-  if fn.empty(fn.glob(path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', path })
-    cmd('packadd packer.nvim')
-    return true
-  end
-
-  return false
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
 end
 
-local started = setup()
+vim.opt.rtp:prepend(lazypath)
 
-local ok, packer = pcall(require, 'packer')
+require('lazy').setup({
+  'nvim-lua/plenary.nvim',
+  'editorconfig/editorconfig-vim',
+  'nvim-lualine/lualine.nvim',
+  'nvim-telescope/telescope.nvim',
+  'nvim-tree/nvim-tree.lua',
+  'nvim-treesitter/nvim-treesitter',
+  'shaunsingh/nord.nvim'
+})
 
-if not ok then
-  return
-end
+require('lualine').setup({})
+require('nvim-tree').setup({})
+require('telescope').setup({})
+require('nvim-treesitter.configs').setup({})
 
-packer.startup(function(use)
-  use { 'akinsho/toggleterm.nvim' }
-  use { 'editorconfig/editorconfig-vim' }
-  use { 'folke/which-key.nvim' }
-  use { 'hrsh7th/nvim-cmp' }
-  use { 'hrsh7th/cmp-nvim-lsp' }
-  use { 'lewis6991/gitsigns.nvim' }
-  use { 'mfussenegger/nvim-dap' }
-  use { 'mfussenegger/nvim-jdtls' }
-  use { 'neovim/nvim-lspconfig' }
-  use { 'norcalli/nvim-colorizer.lua' }
-  use { 'nvim-lua/plenary.nvim' }
-  use { 'nvim-lualine/lualine.nvim' }
-  use { 'nvim-telescope/telescope.nvim' }
-  use { 'nvim-tree/nvim-tree.lua' }
-  use { 'nvim-tree/nvim-web-devicons' }
-  use { 'nvim-treesitter/nvim-treesitter' }
-  use { 'shaunsingh/nord.nvim' }
-  use { 'wbthomason/packer.nvim' }
+local mapleader = ' '
 
-  if started then
-    packer.sync()
-  end
-end)
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', mapleader, '<Nop>', opts)
+
+vim.g.mapleader = mapleader
+
+vim.keymap.set('v', 'p', '"_dP', opts)
+vim.keymap.set('n', '<leader>1', ':NvimTreeToggle <cr>', opts)
+vim.keymap.set('n', '<leader>rg', ':Telescope live_grep<cr>', opts)
+vim.keymap.set('n', '<leader><C-l>.', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+vim.keymap.set('n', '<leader><C-l>d', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+vim.keymap.set('n', '<leader><C-l>f', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
+vim.keymap.set('n', '<leader><C-l>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_disable_backgrounds = true
+vim.g.nord_cursorline_transparent = true
+vim.g.nord_enable_sidebar_background = true
+vim.g.nord_italic = true
+vim.g.nord_uniform_diff_background = true
+vim.g.nord_bold = true
+
+vim.cmd("colorscheme nord")
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.cursorline = true
+vim.opt.encoding = 'utf-8'
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.history = 200
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.sidescrolloff = 8
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'full'
+vim.opt.wrap = false
