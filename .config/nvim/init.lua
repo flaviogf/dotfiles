f75c4034f518b2b1b377a17bd0642ab25dfc526d
flaviogf <flavio.fernandes6@gmail.com>
2023-12-07 08:32:34 -0300

    neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,7 +13,6 @@ require('lazy').setup({
   'nvim-telescope/telescope.nvim',
   'nvim-tree/nvim-tree.lua',
   'nvim-treesitter/nvim-treesitter',
-  'shaunsingh/nord.nvim'
 })
 
 require('lualine').setup({})
@@ -37,20 +36,6 @@ vim.keymap.set('n', '<leader><C-l>.', '<cmd>lua vim.lsp.buf.code_action()<cr>', 
 vim.keymap.set('n', '<leader><C-l>d', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
 vim.keymap.set('n', '<leader><C-l>f', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
 vim.keymap.set('n', '<leader><C-l>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.g.nord_contrast = true
-vim.g.nord_borders = true
-vim.g.nord_disable_backgrounds = true
-vim.g.nord_cursorline_transparent = true
-vim.g.nord_enable_sidebar_background = true
-vim.g.nord_italic = true
-vim.g.nord_uniform_diff_background = true
-vim.g.nord_bold = true
-
-vim.cmd("colorscheme nord")
 
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.cursorline = true
