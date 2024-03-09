b1239ef630bef307e58b35eee25eed838342aead
flaviogf <flavio.fernandes6@gmail.com>
2024-03-09 12:23:35 -0300

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,21 @@
+fish_add_path ~/.local/bin
+
 set fish_greeting
 
-set -x VISUAL "nvim"
+set -x DOCKER_BUILDKIT 1
 set -x EDITOR "nvim"
+set -x GPG_TTY $(tty)
+set -x VISUAL "nvim"
 
 alias df="df -h"
-
 alias find="fd"
-
 alias free="free -m"
-
 alias grep="rg"
-
 alias la="exa --all --group-directories-first --icons"
 alias ll="exa --long --group-directories-first --icons"
 alias ls="exa --all --group --header --long --group-directories-first --icons"
 alias lt="exa --all --tree --group-directories-first --icons"
-
 alias vi="nvim"
 alias vim="nvim"
 
 starship init fish | source
-
-set -x DOCKER_BUILDKIT 1
-set -x GPG_TTY $(tty)
-
-source ~/.asdf/asdf.fish
-
-if [ -f '/home/flaviogf/dev/google-cloud-sdk/path.fish.inc' ]; . '/home/flaviogf/dev/google-cloud-sdk/path.fish.inc'; end
