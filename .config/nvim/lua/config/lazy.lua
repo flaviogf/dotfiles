8006cea20b492a2a4809c23b472f95ccdb774269
flaviogf <flavio.fernandes6@gmail.com>
2024-11-10 15:50:03 -0300

    neovim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,7 +16,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
+    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,4 @@
 return {
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
   { "folke/noice.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "garymjr/nvim-snippets", enabled = false },
 }
