535d029719ca804611e1a8067c0a075e503cbc19
flaviogf <flavio.fernandes6@gmail.com>
2025-05-03 12:09:06 -0300

    wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,10 +4,9 @@ local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
 config.color_scheme = 'Tokyo Night'
-config.default_cursor_style = 'BlinkingBar'
 config.enable_tab_bar = false
 config.font = wezterm.font('JetBrains Mono', { weight = 'Regular' })
-config.font_size = 16
+config.font_size = 14
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'RESIZE'
 
